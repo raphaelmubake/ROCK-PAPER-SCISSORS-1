513f944efb179b8f120edfd7f168e2fcc54d70dc
raphaelmubake <rmubake@gmail.com>
2022-08-05 12:45:14 +0930

    final version

--- Referee.h
+++ Referee.h
@@ -1,15 +1,15 @@
-#include "Human.h"
-#include "Computer.h"
-
 #ifndef REFEREE_H
 #define REFEREE_H
+
+#include "Human.h"
+#include "Computer.h"
 
 class Referee { 
 
     public:
 
     Referee( ); // constructor
-    
+
     char refGame(Human Humanplayer, Computer Computerplayer);
 };
 
